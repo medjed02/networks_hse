8b15702183c6867385da998eb3d667bd61890a5f
Kirill Malyaev <kirill.maljaev@gmail.com>
2024-06-15 23:00:29 +0300

    Add lab2
    add lab2

---
+++ lab2/find_mtu.py
@@ -0,0 +1,67 @@
+import subprocess
+import argparse
+import platform
+
+
+HEADER_SIZE = 28
+MIN_MTU = 0
+MAX_MTU = 65536
+
+
+def check_availability(hostname: str) -> bool:
+    if platform.system().lower() == 'windows':
+        command = f"ping -n 5 {hostname}"
+    else:
+        command = f"ping -n 5 {hostname}"
+
+    try:
+        result = subprocess.run(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return result.returncode == 0
+    except Exception as e:
+        return False
+
+
+def ping_with_packet_size(hostname: str, packet_size: int) -> bool:
+    if platform.system().lower() == "windows":
+        command = f"ping {hostname} -f -l {packet_size}"
+    else:
+        command = f"ping {hostname} -D -c 1 -s {packet_size}"
+
+    try:
+        result = subprocess.run(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return result.returncode == 0
+    except Exception as e:
+        return False
+
+
+def find_min_mtu(hostname: str) -> int:
+    low = MIN_MTU
+    high = MAX_MTU - HEADER_SIZE
+
+    while low + 1 < high:
+        mid = (low + high) // 2
+        if ping_with_packet_size(hostname, mid):
+            low = mid
+        else:
+            high = mid
+
+    return low
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, required=True, help="hostname")
+    args = parser.parse_args()
+
+    hostname = args.host
+    if not check_availability(hostname):
+        print(f"Host with name {hostname} is not available :(")
+        exit(1)
+
+    print("Start finding of minimum MTU...")
+    mtu = find_min_mtu(hostname)
+    mtu += HEADER_SIZE
+    print(f"Minimum MTU = {mtu}")
+
